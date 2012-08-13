48ba0986bbf73b85c69ec02334ba742ff064bff6
snaky <snaky@aboutecho.com>
2012-08-13 21:21:54 +0400

    tabs -> spaces

--- src/com/lingvapps/quizword/MainMenuActivity.java
+++ src/com/lingvapps/quizword/MainMenuActivity.java
@@ -6,17 +6,12 @@ import org.json.JSONObject;
 
 import de.marcreichelt.android.RealViewSwitcher;
 
-import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -35,116 +30,119 @@ import android.widget.Toast;
  * - new Object() { ... }
  */
 
-public class MainMenuActivity extends ListActivity
-{
-	private static RealViewSwitcher realViewSwitcher = null;
+public class MainMenuActivity extends ListActivity {
+    private static RealViewSwitcher realViewSwitcher = null;
 
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        //if (null == savedInstanceState) {
-        	setContentView(R.layout.main_menu);
-        	String[] values = new String[] {
-        			"View My Sets",
-        			"Find Public Sets",
-        			"Account Settings"
-        	};
-        	ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-        			android.R.layout.simple_list_item_1, android.R.id.text1, values);
-			setListAdapter(adapter);
-        //} else {
-        	/*
-		    requestWindowFeature(Window.FEATURE_NO_TITLE);
-		    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-		            WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		    if (MainMenuActivity.realViewSwitcher == null) {
-//		    	renderCards();
-		    }
-    		//MainActivity.realViewSwitcher.setCurrentScreen(current_card);
-			// TODO: take control to another activity
-		    setContentView(MainMenuActivity.realViewSwitcher);
-		    */
-        //}
+
+        // if (null == savedInstanceState) {
+        setContentView(R.layout.main_menu);
+        String[] values = new String[] { "View My Sets", "Find Public Sets",
+                "Account Settings" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        setListAdapter(adapter);
+        // } else {
+        /*
+         * requestWindowFeature(Window.FEATURE_NO_TITLE);
+         * getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+         * WindowManager.LayoutParams.FLAG_FULLSCREEN); if
+         * (MainMenuActivity.realViewSwitcher == null) { // renderCards(); }
+         * //MainActivity.realViewSwitcher.setCurrentScreen(current_card); //
+         * TODO: take control to another activity
+         * setContentView(MainMenuActivity.realViewSwitcher);
+         */
+        // }
     }
 
+    @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
-		if (position == 0) {
-			//renderCards();
-//			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-			Intent myIntent = new Intent(l.getContext(), CardActivity.class);
+        if (position == 0) {
+            // renderCards();
+            // setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            Intent myIntent = new Intent(l.getContext(), CardActivity.class);
             startActivityForResult(myIntent, 0);
-		} else {
-			String item = (String) this.getListAdapter().getItem(position);
-			Toast.makeText(getApplicationContext(), item + " selected", Toast.LENGTH_LONG).show();
-		}
+        } else {
+            String item = (String) this.getListAdapter().getItem(position);
+            Toast.makeText(getApplicationContext(), item + " selected",
+                    Toast.LENGTH_LONG).show();
+        }
     }
-    
-	private void renderCards() {
-		// TODO: Add cache and use it when changing screen orientation
-		try {
-			QuizletReaderTask task = new QuizletReaderTask();
-			task.setObserver(new QuizletReaderTask.Callback() {
 
-				public void onFailure() {
+    private void renderCards() {
+        // TODO: Add cache and use it when changing screen orientation
+        try {
+            QuizletReaderTask task = new QuizletReaderTask();
+            task.setObserver(new QuizletReaderTask.Callback() {
 
-				}
+                public void onFailure() {
 
-				public void onComplete(JSONObject JSON) {
-					try {
-						buildCards(JSON);
-						setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-					} catch (JSONException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			});
-			task.execute("https://api.quizlet.com/2.0/sets/12868186?client_id=Sb54cREqMM&whitespace=1");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-        
+                }
+
+                public void onComplete(JSONObject JSON) {
+                    try {
+                        buildCards(JSON);
+                        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                    } catch (JSONException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            });
+            task.execute("https://api.quizlet.com/2.0/sets/12868186?client_id=Sb54cREqMM&whitespace=1");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private void buildCards(JSONObject JSON) throws JSONException {
-        
-    	MainMenuActivity.realViewSwitcher = new RealViewSwitcher(getApplicationContext());
-		
-		JSONArray terms = (JSONArray) JSON.get("terms");
-		
-		// add some views to it
-		JSONObject t;
-		for (int i = 0; i < terms.length(); i++) {
-			t = terms.getJSONObject(i);
-			RelativeLayout layout = (RelativeLayout) getLayoutInflater().inflate(R.layout.card, null);
-			TextView textView1 = (TextView) layout.findViewById(R.id.card_term);
-			TextView textView2 = (TextView) layout.findViewById(R.id.card_definition);
-			textView1.setText(t.getString("term"));
-			textView2.setText(t.getString("definition"));
-			//textView.setTextSize(30);
-			//textView.setTextColor(Color.BLACK);
-			//textView.setGravity(Gravity.CENTER);
-			MainMenuActivity.realViewSwitcher.addView(layout);
-		}
-		
-		MainMenuActivity.realViewSwitcher.setOnScreenSwitchListener(onScreenSwitchListener);
+
+        MainMenuActivity.realViewSwitcher = new RealViewSwitcher(
+                getApplicationContext());
+
+        JSONArray terms = (JSONArray) JSON.get("terms");
+
+        // add some views to it
+        JSONObject t;
+        for (int i = 0; i < terms.length(); i++) {
+            t = terms.getJSONObject(i);
+            RelativeLayout layout = (RelativeLayout) getLayoutInflater()
+                    .inflate(R.layout.card, null);
+            TextView textView1 = (TextView) layout.findViewById(R.id.card_term);
+            TextView textView2 = (TextView) layout
+                    .findViewById(R.id.card_definition);
+            textView1.setText(t.getString("term"));
+            textView2.setText(t.getString("definition"));
+            // textView.setTextSize(30);
+            // textView.setTextColor(Color.BLACK);
+            // textView.setGravity(Gravity.CENTER);
+            MainMenuActivity.realViewSwitcher.addView(layout);
+        }
+
+        MainMenuActivity.realViewSwitcher
+                .setOnScreenSwitchListener(onScreenSwitchListener);
     }
-    
-	private final RealViewSwitcher.OnScreenSwitchListener onScreenSwitchListener = new RealViewSwitcher.OnScreenSwitchListener() {
-		
-		public void onScreenSwitched(int screen) {
-			// this method is executed if a screen has been activated, i.e. the screen is completely visible
-			//  and the animation has stopped (might be useful for removing / adding new views)
-			Log.d("RealViewSwitcher", "switched to screen: " + screen);
-		}
-		
-	};
-    
+
+    private final RealViewSwitcher.OnScreenSwitchListener onScreenSwitchListener = new RealViewSwitcher.OnScreenSwitchListener() {
+
+        public void onScreenSwitched(int screen) {
+            // this method is executed if a screen has been activated, i.e. the
+            // screen is completely visible
+            // and the animation has stopped (might be useful for removing /
+            // adding new views)
+            Log.d("RealViewSwitcher", "switched to screen: " + screen);
+        }
+
+    };
+
+    @Override
     public void onSaveInstanceState(Bundle outState) {
-//    	if (MainActivity.realViewSwitcher != null) {
-//    		outState.putInt("current_card", MainActivity.realViewSwitcher.getCurrentScreen());
-//    	}
+        // if (MainActivity.realViewSwitcher != null) {
+        // outState.putInt("current_card",
+        // MainActivity.realViewSwitcher.getCurrentScreen());
+        // }
     }
 }
--- src/com/lingvapps/quizword/QuizletReaderTask.java
+++ src/com/lingvapps/quizword/QuizletReaderTask.java
@@ -1,5 +1,3 @@
-
-
 package com.lingvapps.quizword;
 
 import java.io.BufferedReader;
@@ -13,49 +11,50 @@ import org.json.JSONObject;
 import android.os.AsyncTask;
 
 public class QuizletReaderTask extends AsyncTask<String, Void, JSONObject> {
-	
+
     public interface Callback {
         void onComplete(JSONObject JSON);
+
         void onFailure();
     }
-    
+
     private Set<Callback> callbacks = new HashSet<Callback>();
-    
+
     public void setObserver(Callback cb) {
-    	callbacks.clear();
-    	callbacks.add(cb);
+        callbacks.clear();
+        callbacks.add(cb);
     }
-    
+
     @Override
     protected JSONObject doInBackground(String... URLS) {
-    		URL url;
-    		try {
-    			url = new URL(URLS[0]);
-    			BufferedReader in = new BufferedReader(
-    					new InputStreamReader(url.openStream()));
-    			StringBuilder response = new StringBuilder();
-    			String line;
-    			while ((line = in.readLine()) != null)
-    				response.append(line);
-    			in.close();
-    			JSONObject JSON = new JSONObject(response.toString());
-    			return JSON;
-    		} catch (Exception e) {
-    			this.cancel(true);
-    			e.printStackTrace();
-    			return null;
-    		}
+        URL url;
+        try {
+            url = new URL(URLS[0]);
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    url.openStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = in.readLine()) != null)
+                response.append(line);
+            in.close();
+            JSONObject JSON = new JSONObject(response.toString());
+            return JSON;
+        } catch (Exception e) {
+            this.cancel(true);
+            e.printStackTrace();
+            return null;
+        }
     }
-	
-	protected void onPostExecute(JSONObject JSON) {
-	     if (null != JSON) {
-	    	 for (Callback cb: callbacks) {
-	    		 cb.onComplete(JSON);
-	    	 }
-	     } else {
-	         for (Callback cb: callbacks) {
-	             cb.onFailure();
-	         }       
-	     }
-	}
+
+    protected void onPostExecute(JSONObject JSON) {
+        if (null != JSON) {
+            for (Callback cb : callbacks) {
+                cb.onComplete(JSON);
+            }
+        } else {
+            for (Callback cb : callbacks) {
+                cb.onFailure();
+            }
+        }
+    }
 }
